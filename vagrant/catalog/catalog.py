e53bb9124fae91f84e253ee87a76dd14ee2acfb6
Zachary Dumas <zckdms@gmail.com>
2019-06-01 08:02:25 -0500

    format: bring in line with pep8 standard

--- vagrant/catalog/catalog.py
+++ vagrant/catalog/catalog.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python2
 #
 #
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash, make_response
+from flask import (
+    Flask, render_template, request, redirect,
+    jsonify, url_for, flash, make_response
+    )
 from flask import session as login_session
 
 from sqlalchemy import create_engine, asc, desc
@@ -18,47 +21,81 @@ import json
 app = Flask(__name__)
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///catalog.db', connect_args={'check_same_thread': False})
+engine = create_engine(
+    'sqlite:///catalog.db', connect_args={'check_same_thread': False}
+    )
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Displays the description of an item if an item is provited, otherwise displays a list of items 
-# within the requested category if a category is provided, otherwise displays the latest 10 items
+scope = 'https://www.googleapis.com/auth/userinfo.email'
+scope = scope + ' https://www.googleapis.com/auth/userinfo.profile'
+# Displays the description of an item if an item is provited,
+# otherwise displays a list of item within the requested category
+# if a category is provided, otherwise displays the latest 10 items
+
+
 @app.route('/')
 @app.route('/<category>')
 @app.route('/<category>/<item>')
-def showCategory(category = None, item = None):
-    query = session.query(CatalogItem.category.distinct().label('category')).order_by(asc(CatalogItem.category))
+def showCategory(category=None, item=None):
+    query = session.query(
+        CatalogItem.category.distinct().label('category')).order_by(asc(
+            CatalogItem.category
+        )
+        )
     categories = [row.category for row in query.all()]
-    if category != None:
-        items = session.query(CatalogItem).filter_by(category = category).order_by(asc(CatalogItem.name))
+    if category is not None:
+        items = session.query(CatalogItem).filter_by(
+            category=category).order_by(asc(CatalogItem.name))
     else:
-        items = session.query(CatalogItem).order_by(desc(CatalogItem.id)).limit(10).all()
-    if item != None:
-        item = session.query(CatalogItem).filter_by(category = category, name = item).limit(1).one()
-    return render_template('catalog_new.html', categories = categories, entry = item, items = items, login_session = login_session)
+        items = session.query(CatalogItem).order_by(
+            desc(CatalogItem.id)).limit(10).all()
+    if item is not None:
+        item = session.query(CatalogItem).filter_by(
+            category=category, name=item).limit(1).one()
+    return render_template(
+        'catalog_new.html', categories=categories,
+        entry=item, items=items, login_session=login_session
+        )
 
-# GET displays the page to create a new item, POST adds a new item to the database
-@app.route('/new', methods = ['GET','POST'])
+# GET displays the page to create a new item,
+# POST adds a new item to the database
+
+
+@app.route('/new', methods=['GET', 'POST'])
 def createItem():
     if request.method == 'POST':
-        if login_session['email'] == None:
+        if login_session['email'] is not None:
+            newItem = CatalogItem(
+                name=request.form['name'],
+                description=request.form['description'],
+                category=request.form['category'],
+                user_email=login_session['email']
+                )
+            session.add(newItem)
+            session.commit()
+            flash(request.form['name'] + " created.")
+            return redirect(url_for(
+                'showCategory',
+                category=request.form['category'],
+                item=request.form['name'])
+                )
+        else:
             flash("Error: You must be logged in to create an item.")
             return redirect(url_for('showCategory'))
-        newItem = CatalogItem(name = request.form['name'], description = request.form['description'], category = request.form['category'],user_email = login_session['email'])
-        session.add(newItem)
-        session.commit()
-        flash(request.form['name'] + " created.")
-        return redirect(url_for('showCategory', category = request.form['category'], item = request.form['name']))
     else:
         return render_template('createItem.html')
 
 # GET displays the page to edit a item, POST edits an item in the database
-@app.route('/<category>/<item>/edit', methods = ['GET','POST'])
-def editItem(category,item):
+
+
+@app.route('/<category>/<item>/edit', methods=['GET', 'POST'])
+def editItem(category, item):
     if request.method == 'POST':
-        item = session.query(CatalogItem).filter_by(category = category, name = item).limit(1).one()
+        item = session.query(CatalogItem).filter_by(
+            category=category, name=item
+            ).limit(1).one()
         if login_session['email'] == item.user_email:
             item.name = request.form['name']
             item.description = request.form['description']
@@ -67,19 +104,31 @@ def editItem(category,item):
             flash(request.form['name'] + " edited.")
             return redirect(url_for('showCategory'))
         else:
-            flash("Error: You must be logged in and have created an item to edit that item.")
+            flash(
+                '''Error: You must be logged in and
+                 have created an item to edit that item.'''
+                )
             return redirect(url_for('showCategory'))
     else:
-        item = session.query(CatalogItem).filter_by(category = category, name = item).limit(1).one()
-        return render_template('editItem.html', item = item)
+        item = session.query(CatalogItem).filter_by(
+            category=category, name=item
+            ).limit(1).one()
+        return render_template('editItem.html', item=item)
 
 # GET displays a confirmation page to delete an item, POST deletes the item
-@app.route('/<category>/<item>/delete', methods = ['GET','POST'])
-def deleteItem(category,item):
+
+
+@app.route('/<category>/<item>/delete', methods=['GET', 'POST'])
+def deleteItem(category, item):
     if request.method == 'POST':
         itemName = item
-        if session.query(CatalogItem).filter_by(category = category, name = item).limit(1).count() > 0:
-            item = session.query(CatalogItem).filter_by(category = category, name = item).limit(1).one()
+        if session.query(CatalogItem).filter_by(
+                category=category, name=item
+                ).limit(1).count() > 0:
+            item = session.query(CatalogItem).filter_by(
+                category=category,
+                name=item
+                ).limit(1).one()
         else:
             flash(item + " not found")
             return redirect(url_for('showCategory'))
@@ -89,13 +138,18 @@ def deleteItem(category,item):
             flash(itemName + " deleted.")
             return redirect(url_for('showCategory'))
         else:
-            flash("Error: Items can only be deleted by the user who made them.")
+            flash(
+                '''Error: Items can only be deleted
+                 by the user who made them.'''
+            )
             return redirect(url_for('showCategory'))
     else:
-        return render_template('deleteItem.html', category = category, item = item)
+        return render_template('deleteItem.html', category=category, item=item)
 
 # API -----------------------------------------------------
 # GET returns a json object of the categories
+
+
 @app.route('/api/v1')
 def catalogJson():
     query = session.query(CatalogItem).order_by(asc(CatalogItem.name))
@@ -103,38 +157,59 @@ def catalogJson():
     return jsonify([item.serialize for item in items])
 
 # GET returns a json object of the items within a category
+
+
 @app.route('/api/v1/<category>')
 def categoryJson(category):
-    query = session.query(CatalogItem).filter_by(category = category).order_by(asc(CatalogItem.name))
+    query = session.query(CatalogItem).filter_by(
+        category=category
+        ).order_by(asc(CatalogItem.name))
     items = query.all()
     return jsonify([item.serialize for item in items])
 
 # GET returns a json object of an item
+
+
 @app.route('/api/v1/<category>/<item>')
-def itemJson(category,item):
-    item = session.query(CatalogItem).filter_by(category = category, name = item).one()
+def itemJson(category, item):
+    item = session.query(CatalogItem).filter_by(
+        category=category, name=item
+        ).one()
     return jsonify(item.serialize)
 
 # Authentication --------------------------------------------
 # redirect the user to OAuth provider
+
+
 @app.route('/login')
 def login():
-    flow = flow_from_clientsecrets('client_secrets.json', scope='https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',redirect_uri=url_for('connect', _external=True))
+    flow = flow_from_clientsecrets(
+        'client_secrets.json',
+        scope=scope,
+        redirect_uri=url_for('connect', _external=True)
+        )
     return redirect(flow.step1_get_authorize_url())
 
-# POST logs the user in 
-@app.route('/connect', methods = ['Get','POST'])
+# POST logs the user in
+
+
+@app.route('/connect', methods=['Get', 'POST'])
 def connect():
-    flow = flow_from_clientsecrets('client_secrets.json', scope='https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',redirect_uri=url_for('connect', _external=True))
+    flow = flow_from_clientsecrets(
+        'client_secrets.json',
+        scope=scope,
+        redirect_uri=url_for('connect', _external=True))
     # exchange the token for credentials
     try:
         code = request.args.get('code')
-        if code == None:
-            return "access was denied"     
-        credentials = flow.step2_exchange(code)
+        if code is not None:
+            credentials = flow.step2_exchange(code)
+        else:
+            return "access was denied"
     except FlowExchangeError:
         return "failed to aqcuire authorization"
-    # use the credentials to get the users information and store it in the login_session
+    # use the credentials to get the users information
+    # and store it in the login_session
     userinfo_url = "https://www.googleapis.com/oauth2/v2/userinfo"
     params = {'access_token': credentials.access_token}
     answer = requests.get(userinfo_url, params=params)
@@ -147,7 +222,9 @@ def connect():
     return redirect(url_for('showCategory'))
 
 # POST logs a user out
-@app.route('/disconnect', methods = ['POST','GET'])
+
+
+@app.route('/disconnect', methods=['POST', 'GET'])
 def disconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
